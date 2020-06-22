31d49757d700a02d074eaa7f620948ace78d1ca0
Maximiliano <maximilianodacruz@gmail.com>
2020-06-22 07:21:11 -0300

    add suites

---
+++ src/test/java/br/com/restassuredapitesting/runners/Acceptance.java
@@ -0,0 +1,19 @@
+package br.com.restassuredapitesting.runners;
+
+import br.com.restassuredapitesting.tests.auth.tests.PostAuthTest;
+import br.com.restassuredapitesting.tests.base.tests.BaseTest;
+import br.com.restassuredapitesting.tests.booking.tests.GetBookingTest;
+import br.com.restassuredapitesting.tests.booking.tests.PutBookingTest;
+import org.junit.experimental.categories.Categories;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Categories.class)
+@Categories.IncludeCategory(br.com.restassuredapitesting.suites.Acceptance.class)
+@Suite.SuiteClasses({
+        PostAuthTest.class,
+        GetBookingTest.class,
+        PutBookingTest.class,
+})
+public class Acceptance {
+}
